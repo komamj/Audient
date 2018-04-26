90199d1be604dc5382c5b1f8b30c16e33295f24e
Mao Jun <maojun@tp-link.com.cn>
2018-04-26 14:48:38 +0800

    Audient: Add search filter keyword.

--- app/src/main/java/com/xinshang/audient/search/SearchPresenter.java
+++ app/src/main/java/com/xinshang/audient/search/SearchPresenter.java
@@ -57,7 +57,7 @@ public class SearchPresenter implements SearchContract.Presenter {
 
         mDisposables = new CompositeDisposable();
 
-        mBlacklist.addAll(Arrays.asList("DJ/舞曲/DISCO/迪斯科/卡拉/OK/蹦/劲爆/舞/DANCE/节奏/嗨/HIGH/REMIX/MIX/摇/DOWNTEMPO/滚/ROCK/说唱/RAP/嘻哈/动次/打次/打碟/电音/控/午夜/打/敲/BOX/哀/葬/FUNERAL/棺/蛊/灵/魂/SOUL/诅/咒/呪/亡/凶/噩/喊麦/MC/神曲/串烧/畜/鬼/朋克/PUNK/金属/METAL/雷鬼/REGGAE/恐怖/惊悚/TERROR/HORRIBLE/思春/性爱/广场/大妈/二人/戏/曲/剧/腔/调/奏/鸣/二胡/唢呐/进行/解放军/民/红/军/国/兵/警察/公安/疆/藏/抗战/抗日/救国/义勇军进行曲/党/共产/主义/中国/太平/盛世/新时代/中国梦/复兴/崛起/族/雷/歌/东北/佛教/宗教/咒/经/诵/释/法师/道长/尼姑/活佛/施主/祷告/祈祷/真主/耶稣/基督/伊斯兰/BLESS/GOD/综艺/歌手/梦想的声音/季/期/春晚/卫视/铃声/选秀/游戏/原创/合唱/歌/学/呻/吟/叫/联唱/连唱/style/川话/方言/高音/年会/节目/电台/朗/龙/党".split("/")));
+        mBlacklist.addAll(Arrays.asList("DJ/舞曲/DISCO/迪斯科/卡拉/OK/蹦/劲爆/舞/DANCE/节奏/嗨/HIGH/REMIX/MIX/摇/DOWNTEMPO/滚/ROCK/说唱/RAP/嘻哈/动次/打次/打碟/电音/控/午夜/打/敲/BOX/哀/葬/FUNERAL/棺/蛊/灵/魂/SOUL/诅/咒/呪/亡/凶/噩/喊麦/MC/神曲/串烧/畜/鬼/朋克/PUNK/金属/METAL/雷鬼/REGGAE/恐怖/惊悚/TERROR/HORRIBLE/思春/性爱/广场/大妈/二人/戏/曲/剧/腔/调/奏/鸣/二胡/唢呐/进行/解放军/民/红/军/国/兵/警察/公安/疆/藏/抗战/抗日/救国/义勇军进行曲/党/共产/主义/中国/太平/盛世/新时代/中国梦/复兴/崛起/族/雷/歌/东北/佛教/宗教/咒/经/诵/释/法师/道长/尼姑/活佛/施主/祷告/祈祷/真主/耶稣/基督/伊斯兰/BLESS/GOD/综艺/歌手/梦想的声音/季/期/春晚/卫视/铃声/选秀/游戏/原创/合唱/歌/学/呻/吟/叫/联唱/连唱/style/川话/方言/高音/年会/节目/电台/朗/龙/党/999/666/233".split("/")));
     }
 
     @Inject
@@ -209,6 +209,12 @@ public class SearchPresenter implements SearchContract.Presenter {
 
         String keyword = word.trim().toUpperCase();
 
-        return mBlacklist.contains(keyword);
+        for (String invalidKeyword : mBlacklist) {
+            if (keyword.contains(invalidKeyword)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
