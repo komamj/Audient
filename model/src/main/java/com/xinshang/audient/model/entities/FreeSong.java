31063e3c829455b6e2c5681c36759e58910d3f62
Mao Jun <maojun@tp-link.com.cn>
2018-05-14 17:22:13 +0800

    Audient: Modify free song.

--- model/src/main/java/com/xinshang/audient/model/entities/FreeSong.java
+++ model/src/main/java/com/xinshang/audient/model/entities/FreeSong.java
@@ -1,13 +1,24 @@
 package com.xinshang.audient.model.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 public class FreeSong {
+    @SerializedName("cuponId")
     public String cuponId;
+    @SerializedName("storeId")
     public String storeId;
+    @SerializedName("mediaId")
     public String mediaId;
+    @SerializedName("mediaName")
     public String mediaName;
+    @SerializedName("mediaInterval")
     public String mediaInterval;
+    @SerializedName("artistId")
     public String artistId;
+    @SerializedName("artistName")
     public String artistName;
+    @SerializedName("albumId")
     public String albumId;
+    @SerializedName("albumName")
     public String albumName;
 }
